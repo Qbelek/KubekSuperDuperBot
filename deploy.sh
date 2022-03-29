4f1d8924e491271470b1df5706dc8ea7ee5286c3
jakub.tomilo <jakub.tomilo@comarch.com>
2022-03-29 12:58:17 +0200

    Create multi app deployment to GAE

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 mvn clean package
-gcloud app deploy ./engine/target/app.yaml ./ui/target/app.yaml
+mkdir deploy
+mkdir deploy/engine
+cp engine-app.yaml deploy/engine/app.yaml
+cp engine/target/*.jar deploy/engine
+mkdir deploy/ui
+cp ui-app.yaml deploy/ui/app.yaml
+cp ui/target/*.jar deploy/ui
+gcloud app deploy deploy/engine/app.yaml deploy/ui/app.yaml --quiet
